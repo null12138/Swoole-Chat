848ddfc3a7773761a799c346827a06321628e0fe
BeyondToSky <1416225834@qq.com>
2019-06-17 02:33:41 +0800

    ws地址配置

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,8 +2,10 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\facade\Config;
 class Index extends Controller{
 	public function index() {
+		$this->assign('ws_host',Config::get('app.ws_host'));
 		return $this->fetch();
 	}
 }
