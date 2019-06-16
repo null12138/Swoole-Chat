dd295cb98a700ba629be6493dcd44309c8fae251
BeyondToSky <1416225834@qq.com>
2019-06-17 01:27:27 +0800

    用户退出未删除对应用户头像

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,9 +2,6 @@
 namespace app\index\controller;
 
 use think\Controller;
-use think\facade\Request;
-use think\facade\Session;
-use think\Db;
 class Index extends Controller{
 	public function index() {
 		return $this->fetch();
